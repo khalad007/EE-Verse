96f42b76274f37da56c3f5c108b3d50088c16886
khalad007 <www.arifhossain49919@gmail.com>
2023-10-18 10:04:51 -0700

    Send Product Data to The Server

--- src/Pages/AddProduct/AddProduct.jsx
+++ src/Pages/AddProduct/AddProduct.jsx
@@ -1,4 +1,8 @@
+import swal from "sweetalert";
+
 const AddProduct = () => {
+
+
     const handleAddProduct = e => {
         e.preventDefault();
 
@@ -14,11 +18,27 @@ const AddProduct = () => {
         const newProduct = { name, rating, brandName, type, price, description, photoURL }
 
         console.log(newProduct);
+
+        //send data to the server
+        fetch('http://localhost:5000/product', {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newProduct)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    swal("Good job!", "New Product Added Successfully.!", "success");
+                }
+            })
     }
 
     return (
         <div className="bg-[#f4f3f0] p-24">
-            <h1 className="font-extrabold text-4xl mb-7 text-center">Add A Product</h1>
+            <h1 className="font-extrabold text-4xl mb-7 text-center">Add A <span className="text-[#247CC6]">Product ..!</span> </h1>
             <form onSubmit={handleAddProduct}>
                 {/* form row name & Type */}
                 <div className="md:flex gap-2">
@@ -56,7 +76,7 @@ const AddProduct = () => {
                             </select>
                         </label>
                     </div>
-                    <div className="form-control md-w-1/2">
+                    <div className="form-control md:w-1/2">
                         <label className="label">
                             <span className="label-text">Price</span>
                         </label>
