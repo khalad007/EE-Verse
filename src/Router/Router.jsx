f8ece0503dbb91974421c9a05b152ad94cc2652e
khalad007 <www.arifhossain49919@gmail.com>
2023-10-17 19:36:17 -0700

    Router Setup

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,8 +1,18 @@
-const { createBrowserRouter } = require("react-router-dom");
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../Layouts/Root";
+import Home from "../Pages/Home/Home";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: 
+        element: <Root></Root>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
     }
-])
+]);
+
+export default router;
--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { RouterProvider } from 'react-router-dom';
+import router from './Router/Router.jsx';
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
